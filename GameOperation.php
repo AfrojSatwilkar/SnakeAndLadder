d2023e0969b87b4c89c0e7cf36db40c900ab5ec1
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-12-21 21:00:20 +0530

    Count number of times dice was played to win game and position after every dice role

--- GameOperation.php
+++ GameOperation.php
@@ -21,39 +21,52 @@ class GameOperation {
         $this->option();
     }
 
+    /**
+     * option method is used to get random number betwwen 1 to 3.
+     * call nextMove method and pass this random number.  
+     */
     public function option() {
         $option = rand(1,3);
         $this->nextMove($option);
     }
 
+    /**
+     * nextMove method is used to check for option. 
+     * @param option
+     */
     public function nextMove($option) {
         switch ($option) {
             case 1 :
                 // No play
-                echo "No Play " . $this->startPosition . " location\n";
+                echo "No Play " . $this->count . " Moves and ". $this->startPosition . " location\n";
                 break;
             case 2 :
                 // Ladder
                 $this->startPosition += $this->diceNumber;
+
+                // Player position go above 100 then player stays in the same previous position
                 if ($this->startPosition > 100) {
                     $this->startPosition -= $this->diceNumber;
                 }
-                echo "Next position " . $this->startPosition . "\n";
+                echo $this->count . " Mooves and " . $this->startPosition . " position\n";
                 break;
             case 3 :
                 // Snake
                 $this->startPosition -= $this->diceNumber;
+
+                // Position moves below 0 then the player restart from 0
                 if ($this->startPosition <= 0) {
                     $this->startPosition = 0;
                 }
-                echo "Position " . $this->startPosition . "\n";
+                echo $this->count . " Moves and " . $this->startPosition . " position\n";
                 break;
         }
 
+        // Player position is not equal to 100 then repeat move
         if ($this->startPosition != 100) {
             $this->diceRoll();
         } else {
-            echo "Player won \n";
+            echo "Player won with " . $this->count . " moves.\n";
         }
     }
 }
