6f0f6b0d19f7bd8cfe975f89f9d82aad39e93631
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-12-21 18:10:11 +0530

    commit next move

--- GameOperation.php
+++ GameOperation.php
@@ -1,12 +1,43 @@
 <?php
 class GameOperation {
-    private $startPosition = 0;
+    private $startPosition;
     private $previousPosition;
-    private $count =0;
+    private $count;
+    private $diceNumber;
+
+    public function __construct()
+    {
+        $this->startPosition = 0;
+        $this->count = 0;
+    }
 
     public function diceRoll() {
         $this->previousPosition = $this->startPosition;
-        $this->startPosition = rand(1,6);
+        $this->diceNumber = rand(1,6);
+        $this->count++;
+        $this->option();
+    }
+
+    public function option() {
+        $option = rand(1,3);
+        $this->nextMove($option);
+    }
+
+    public function nextMove($option) {
+        switch ($option) {
+            case 1 :
+                // No play
+                echo "No Play " . $this->startPosition . " location\n";
+                break;
+            case 2 :
+                $this->startPosition += $this->diceNumber;
+                echo "Next position " . $this->startPosition;
+                break;
+            case 3 :
+                $this->startPosition -= $this->diceNumber;
+                echo "Position " . $this->startPosition;
+                break;
+        }
     }
 }
 
